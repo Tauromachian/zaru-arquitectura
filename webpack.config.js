bca32a8e3270ebd41ea363808a268fd7832a6b2c
Jose Garcia <jogarcia@uclv.cu>
2019-10-31 16:11:14 -0700

    Change webpack entry and output

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./public/javascripts/src/app.js",
+  entry: "./resources/index.js",
   output: {
     path: path.join(__dirname, "/public/dist"),
     publicPath: "/public/dist",
