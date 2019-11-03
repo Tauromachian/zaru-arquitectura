8cfb0da1c6866856ce9aed30e0b3d4f7eb790d1a
Jose Garcia <jogarcia@uclv.cu>
2019-11-03 14:25:41 -0800

    Put mystyle down bootstrap styles

--- resources/index.js
+++ resources/index.js
@@ -1,6 +1,6 @@
 import 'bootstrap/js/dist/collapse';
-import "./stylesheets/style.scss";
 import "bootstrap/scss/bootstrap.scss";
+import "./stylesheets/style.scss";
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
