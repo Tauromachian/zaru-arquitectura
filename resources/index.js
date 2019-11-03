7b9b4ed2dd90c3548add8aba997fcc96c9ec1ecc
Jose Garcia <jogarcia@uclv.cu>
2019-11-02 23:18:32 -0700

    Import fontawesome

--- resources/index.js
+++ resources/index.js
@@ -1,3 +1,7 @@
 import 'bootstrap/js/dist/collapse';
 import "./stylesheets/style.scss";
 import "bootstrap/scss/bootstrap.scss";
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
