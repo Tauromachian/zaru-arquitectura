5e73eee1754d64ac2e5bef55dc23597c21fae683
Jose Garcia <jogarcia@tuta.io>
2022-04-30 15:17:10 -0400

    Load bootstrap styles on the scss

--- resources/index.js
+++ resources/index.js
@@ -1,8 +1,7 @@
-import 'bootstrap/js/dist/collapse';
-import "bootstrap/scss/bootstrap.scss";
+import "bootstrap/js/dist/collapse";
 import "./stylesheets/style.scss";
-import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/js/regular';
-import '@fortawesome/fontawesome-free/js/brands';
-import '@fortawesome/fontawesome-free/js/v4-shims';
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
+import "@fortawesome/fontawesome-free/js/v4-shims";
