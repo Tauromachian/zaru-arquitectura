9212b5694376155d70356571feaec409bc979138
Jose Garcia <jogarcia@tuta.io>
2022-04-30 15:49:35 -0400

    Remove font awesome module

--- resources/index.js
+++ resources/index.js
@@ -1,7 +1,2 @@
 import "bootstrap/js/dist/collapse";
 import "./stylesheets/style.scss";
-import "@fortawesome/fontawesome-free/js/fontawesome";
-import "@fortawesome/fontawesome-free/js/solid";
-import "@fortawesome/fontawesome-free/js/regular";
-import "@fortawesome/fontawesome-free/js/brands";
-import "@fortawesome/fontawesome-free/js/v4-shims";
