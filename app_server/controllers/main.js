477f3f19b1d41e6db75410f5e1c8f8bf0ac1875d
Jose Garcia <jogarcia@tuta.io>
2022-05-02 09:59:25 -0400

    Set contact us route and controller

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -2,6 +2,11 @@ const home = (req, res) => {
   return res.render("pages/index", { title: "ZaruArquitectura" });
 };
 
+const contactUs = (req, res) => {
+  return res.render("pages/contact_us", { title: "Contáctanos" });
+};
+
 module.exports = {
-  home
+  home,
+  contactUs,
 };
--- app_server/routes/routes.js
+++ app_server/routes/routes.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const ctrl = require('../controllers/main');
-
+const ctrl = require("../controllers/main");
 
 /* GET home page. */
-router.get('/', ctrl.home);
+router.get("/", ctrl.home);
+router.get("/contactanos", ctrl.contactUs);
 
 module.exports = router;
