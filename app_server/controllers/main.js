a49bb49b918792413e88d216de6b68e603e19dc2
Jose Garcia <jogarcia@tuta.io>
2022-05-02 10:25:57 -0400

    Send route to views

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,9 +1,15 @@
 const home = (req, res) => {
-  return res.render("pages/index", { title: "ZaruArquitectura" });
+  return res.render("pages/index", {
+    title: "ZaruArquitectura",
+    route: "/",
+  });
 };
 
 const contactUs = (req, res) => {
-  return res.render("pages/contact_us", { title: "Contáctanos" });
+  return res.render("pages/contact_us", {
+    title: "Contáctanos",
+    route: "/contactanos",
+  });
 };
 
 module.exports = {
